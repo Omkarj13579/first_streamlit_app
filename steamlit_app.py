7810c52cad09874eb8b8f2ffe5fcb463b8b400f6
Omkarj13579 <168425145+Omkarj13579@users.noreply.github.com>
2024-04-29 16:41:37 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -16,5 +16,5 @@ streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacodo','Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
